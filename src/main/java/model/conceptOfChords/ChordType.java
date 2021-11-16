6386f1fe5b2d4a89ac546bbbc56e06a5087e3d91
rwerner1 <richard.werner@stud.htwk-leipzig.de>
2021-11-16 18:28:38 +0100

    added some chords

--- src/main/java/model/conceptOfChords/ChordType.java
+++ src/main/java/model/conceptOfChords/ChordType.java
@@ -6,14 +6,27 @@ import model.conceptOfScale.ScaleDegree;
 import java.util.Arrays;
 import java.util.List;
 
+import static model.conceptOfScale.ScaleDegree.*;
+
 @Getter
 public enum ChordType {
 
-    MAJOR(Arrays.asList(ScaleDegree.ROOT, ScaleDegree.THIRD, ScaleDegree.FIFTH)),
-    MINOR(Arrays.asList(ScaleDegree.ROOT, ScaleDegree.FLAT_THIRD, ScaleDegree.FIFTH)),
-    MAJOR_SEVENTH(Arrays.asList(ScaleDegree.ROOT, ScaleDegree.THIRD, ScaleDegree.FIFTH, ScaleDegree.SEVENTH)),
-    MINOR_SEVENTH(Arrays.asList(ScaleDegree.ROOT, ScaleDegree.FLAT_THIRD, ScaleDegree.FIFTH, ScaleDegree.FLAT_SEVENTH)),
-    DOMINANT_SEVENTH(Arrays.asList(ScaleDegree.ROOT, ScaleDegree.THIRD, ScaleDegree.FIFTH, ScaleDegree.FLAT_SEVENTH));
+    // triads
+    MAJOR(Arrays.asList(ROOT, NATURAL_THIRD, FIFTH)),
+    MINOR(Arrays.asList(ROOT, FLAT_THIRD, FIFTH)),
+    DIMINISHED(Arrays.asList(ROOT, FLAT_THIRD, FLAT_FIFTH)),
+    AUGMENTED(Arrays.asList(ROOT, NATURAL_THIRD, SHARP_FIFTH)),
+
+    // quartal chords
+    MAJOR_SEVENTH(Arrays.asList(ROOT, NATURAL_THIRD, FIFTH, NATURAL_SEVENTH)),
+    MINOR_SEVENTH(Arrays.asList(ROOT, FLAT_THIRD, FIFTH, FLAT_SEVENTH)),
+    DOMINANT_SEVENTH(Arrays.asList(ROOT, NATURAL_THIRD, FIFTH, FLAT_SEVENTH)),
+
+    HALF_DIMINISHED_SEVENTH(Arrays.asList(ROOT, FLAT_THIRD, FLAT_FIFTH, FLAT_SEVENTH)),
+    FULLY_DIMINISHED_SEVEN(Arrays.asList(ROOT, FLAT_THIRD, FLAT_FIFTH, DOUBLE_DIMINISHED_SEVENTH)),
+
+    MINOR_MAJOR_SEVENTH(Arrays.asList(ROOT, FLAT_THIRD, FIFTH, NATURAL_SEVENTH));
+
 
     private Tonality tonality;
     private List<ScaleDegree> scaleDegrees;
@@ -28,10 +41,10 @@ public enum ChordType {
     }
 
     private Tonality calculateTonality(List<ScaleDegree> scaleDegrees) throws Exception {
-        if(scaleDegrees.contains(ScaleDegree.THIRD) && scaleDegrees.contains(ScaleDegree.FIFTH)){
+        if(scaleDegrees.contains(NATURAL_THIRD) && scaleDegrees.contains(FIFTH)){
             return Tonality.MAJOR;
         }
-        if(scaleDegrees.contains(ScaleDegree.FLAT_THIRD) && scaleDegrees.contains(ScaleDegree.FIFTH)){
+        if(scaleDegrees.contains(FLAT_THIRD) && scaleDegrees.contains(FIFTH)){
             return Tonality.MINOR;
         }
         throw new Exception("Cant calculate Tonality");
